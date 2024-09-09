e305f2bcdaabfdc2a29b88e90521c79d253babb4
eugenio oliveira <57637097+amnesiaoliveira@users.noreply.github.com>
2024-09-09 09:34:00 -0400

    adicionado os favicon's

--- cadastro_produto.php
+++ cadastro_produto.php
@@ -36,6 +36,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Produtos</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="img/favicon_io/site.webmanifest">
     <style>
         body {
             font-family: Arial, sans-serif;
